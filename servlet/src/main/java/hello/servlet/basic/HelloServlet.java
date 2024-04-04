90971087a42a307494468a9f907192746b91f0ef
bllee29 <bllee0209@gmail.com>
2024-04-04 22:58:31 +0900

    request Servlet

---
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override   // 이 서블릿이 호출이 되면 이 서비스 함수가 호출됨
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+}
