81851d2dcf542b413ea6087cea36442bb41db38d
bllee29 <bllee0209@gmail.com>
2024-04-04 21:10:18 +0900

    servlet_init_commit

--- hello_spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ hello_spring/src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -2,12 +2,13 @@ package hello.hello_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
 }
