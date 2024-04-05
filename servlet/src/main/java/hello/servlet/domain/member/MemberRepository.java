e572d1ce3b843633d7d8b5806cd4b5065baa18a3
bllee29 <bllee0209@gmail.com>
2024-04-05 18:36:15 +0900

    servlet application

---
+++ servlet/src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -0,0 +1,44 @@
+package hello.servlet.domain.member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
+ */
+public class MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
+    // static 없어도 싱글톤이라 한개임
+
+    // singleton
+    private static final MemberRepository instance = new MemberRepository();
+
+    private MemberRepository() {
+    }
+
+    public static MemberRepository getInstance() {
+        return instance;
+    }
+
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // 복사해서 리턴해줌
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
